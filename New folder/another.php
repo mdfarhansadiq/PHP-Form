de7dd0bf97a7770f695dd1387ba43d88453625de
Md. Farhan Sadiq <mdfarhansadiq01@gmail.com>
2022-12-17 14:32:43 +0600

    code

--- New folder/another.php
+++ New folder/another.php
@@ -5,10 +5,15 @@ session_start();
 <!DOCTYPE html>
 <html>
 
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+
 <body>
 
     <?php
-    echo $_SESSION["useremail"];
+    // echo $_SESSION["useremail"];
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         session_unset();
@@ -18,9 +23,12 @@ session_start();
     }
     ?>
 
-    <form action="another.php" method="post">
-        <button type="submit">Logout</button>
-    </form>
+    <div class="login-box">
+        <h2>Logout</h2>
+        <form action="another.php" method="post">
+            <button type="submit" style="text-align: center">Logout</button>
+        </form>
+    </div>
 
 </body>
 
