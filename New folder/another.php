062687d47d2aba5c41c84061d19250b5563fb743
Md. Farhan Sadiq <mdfarhansadiq01@gmail.com>
2022-12-17 10:16:35 +0600

    code

---
+++ New folder/another.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<body>
+
+
+    <form action="another.php" method="post">
+        <button type="submit">Logout</button>
+    </form>
+
+</body>
+
+</html>
